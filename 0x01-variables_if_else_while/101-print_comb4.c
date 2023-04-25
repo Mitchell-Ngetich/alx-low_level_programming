abbccabdbede63a6257938a4cc6ba38c3871d7e8
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-04-25 05:59:20 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,19 +8,21 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = 0; x <= 7; x++)
+	for (x = 0; x < 9; x++)
 	{
-		for (y = x + 1; y <= 8; y++)
+		for (y = x + 1; y <= 9; y++)
 		{
 			for (z = y + 1; z <= 9; z++)
 			{
+				if ((x != y) != z)
+				{
 				putchar(x + '0');
 				putchar(y + '0');
 				putchar(z + '0');
-				if (z != 9)
-				{
-					putchar(',');
-					putchar(' ');
+				if  (x == 7 && y == 8)
+				continue;
+				putchar(',');
+				putchar(' ');
 				}
 			}
 		}
